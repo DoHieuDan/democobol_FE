c1d10e48ecdf2440d2835b06960a0852c41299e4
Thanh Phu <thanhphu8436@gmail.com>
2025-02-07 10:20:59 +0700

    add user and delete user

--- src/pages/BMSPage/COUSR03.tsx
+++ src/pages/BMSPage/COUSR03.tsx
@@ -8,263 +8,329 @@ import { GridItem } from '../../components/GridSystem';
 import Input from '../../components/Input';
 
 export default function COUSR03() {
-    
+
     type formInput = {
-        usridin: string,
+        userId: string,
 
     }
 
     type formOutput = {
         cousr03: string,
-cousr3a: string,
-trnname: string,
-title01: string,
-curdate: string,
-pgmname: string,
-title02: string,
-curtime: string,
-fname: string,
-lname: string,
-usrtype: string,
-errmsg: string,
+        cousr3a: string,
+        trnname: string,
+        title01: string,
+        curdate: string,
+        pgmname: string,
+        title02: string,
+        curtime: string,
+        firstName: string,
+        lastName: string,
+        role: string,
+        errmsg: string,
 
     }
-    
+
     const [formData, setFormData] = useState<formInput>(
-    {
-        usridin: '',
+        {
+            userId: '',
 
-    });
+        });
     const [receivedData, setReceivedData] = useState<formOutput>(
-     {
-        cousr03: '',
-cousr3a: '',
-trnname: '',
-title01: '',
-curdate: 'mm/dd/yy',
-pgmname: '',
-title02: '',
-curtime: 'hh:mm:ss',
-fname: '',
-lname: '',
-usrtype: '',
-errmsg: '',
+        {
+            cousr03: '',
+            cousr3a: '',
+            trnname: '',
+            title01: '',
+            curdate: 'mm/dd/yy',
+            pgmname: '',
+            title02: '',
+            curtime: 'hh:mm:ss',
+            firstName: '',
+            lastName: '',
+            role: '',
+            errmsg: '',
 
-    });
+        });
 
     const handleInputChange = (event: ChangeEvent<HTMLInputElement>) => {
-    setFormData((state) => {
-        return {
-        ...state,
-        [event.target.name]: event.target.value,
-        };
-    });
+        setFormData((state) => {
+            return {
+                ...state,
+                [event.target.name]: event.target.value,
+            };
+        });
     };
 
     const handleSubmit = async (event: KeyboardEvent<HTMLInputElement>) => {
-    if (event.key === 'Enter') {
-        for (const key in formData) {
-        if (!formData[key]) {
-            return;
-        }
-        }
+        if (event.key === 'Enter') {
+            if (!formData.userId) {
+                return; // Ngăn request nếu userId trống
+            }
 
-        const response = await axios.post(
-        httpConfig.domain + '/Cousr03',
-        formData
-        );
+            try {
+                const response = await axios.get(`${httpConfig.domain}/api/NV1/${formData.userId}`);
+                // Kiểm tra nếu response có dữ liệu hợp lệ
+                if (response.data.code === 200 && response.data.result) {
+                    const { firstName, lastName, role } = response.data.result;
+                    setReceivedData(prevState => ({
+                        ...prevState,
+                        firstName: firstName.trim(), // Loại bỏ khoảng trắng thừa
+                        lastName: lastName.trim(),
+                        role,
+                        errmsg: '' // Xóa thông báo lỗi nếu có
+                    }));
+                } else {
+                    setReceivedData(prevState => ({
+                        ...prevState,
+                        errmsg: response.data.message || 'User not found'
+                    }));
+                }
+            } catch (error) {
+                console.error("Lỗi khi gửi request:", error);
+                setReceivedData(prevState => ({
+                    ...prevState,
+                    errmsg: 'Lỗi kết nối đến server'
+                }));
+            }
+        }
+        else if (event.key === 'F4') {
+            // Khi nhấn F4 -> Xóa userId và một số trường hiển thị
+            setFormData({ userId: '' });
+            setReceivedData(prevState => ({
+                ...prevState,
+                firstName: '',
+                lastName: '',
+                role: '',
+                errmsg: '',
+            }));
+        }
+        else if (event.key === 'F5') {
+            event.preventDefault();
+            if (!formData.userId) {
+                setReceivedData(prevState => ({
+                    ...prevState,
+                    errmsg: 'User ID is empty'
+                }));
+                return;
+            }
 
-        setReceivedData(_state => response.data);
-    }
+            try {
+                const response = await axios.delete(`${httpConfig.domain}/api/NV1/${formData.userId}`);
+                if (response.data.code === 200) {
+                    setFormData({ userId: '' }); // Xóa userId sau khi xóa user thành công
+                    setReceivedData(prevState => ({
+                        ...prevState,
+                        firstName: '',
+                        lastName: '',
+                        role: '',
+                        errmsg: 'User deleted successfully!',
+                    }));
+                } else {
+                    setReceivedData(prevState => ({
+                        ...prevState,
+                        errmsg: response.data.message || 'Không thể xóa user!'
+                    }));
+                }
+            } catch (error) {
+                console.error("A", error);
+                setReceivedData(prevState => ({
+                    ...prevState,
+                    errmsg: 'Error!'
+                }));
+            }
+        }
     };
-    
-  return (
-    <>
-     
-    <Helmet>
-        <title>COUSR03</title>
-    </Helmet>
-    
-<GridItem col={1} row={1}>
-    <pre style={{color:"#7faded"}}>
-         Tran: 
-    </pre>
-</GridItem>
 
-    
-<GridItem col={7} row={1}>
-    <pre style={{color:"#7faded"}}>
-         {receivedData.trnname } 
-    </pre>
-</GridItem>
 
-    
-<GridItem col={21} row={1}>
-    <pre style={{color:"yellow"}}>
-         {receivedData.title01 } 
-    </pre>
-</GridItem>
 
-    
-<GridItem col={65} row={1}>
-    <pre style={{color:"#7faded"}}>
-         Date: 
-    </pre>
-</GridItem>
 
-    
-<GridItem col={71} row={1}>
-    <pre style={{color:"#7faded"}}>
-         {receivedData.curdate } 
-    </pre>
-</GridItem>
 
-    
-<GridItem col={1} row={2}>
-    <pre style={{color:"#7faded"}}>
-         Prog: 
-    </pre>
-</GridItem>
+    return (
+        <>
 
-    
-<GridItem col={7} row={2}>
-    <pre style={{color:"#7faded"}}>
-         {receivedData.pgmname } 
-    </pre>
-</GridItem>
+            <Helmet>
+                <title>COUSR03</title>
+            </Helmet>
 
-    
-<GridItem col={21} row={2}>
-    <pre style={{color:"yellow"}}>
-         {receivedData.title02 } 
-    </pre>
-</GridItem>
+            <GridItem col={1} row={1}>
+                <pre style={{ color: "#7faded" }}>
+                    Tran:
+                </pre>
+            </GridItem>
 
-    
-<GridItem col={65} row={2}>
-    <pre style={{color:"#7faded"}}>
-         Time: 
-    </pre>
-</GridItem>
 
-    
-<GridItem col={71} row={2}>
-    <pre style={{color:"#7faded"}}>
-         {receivedData.curtime } 
-    </pre>
-</GridItem>
+            <GridItem col={7} row={1}>
+                <pre style={{ color: "#7faded" }}>
+                    {receivedData.trnname}
+                </pre>
+            </GridItem>
 
-    
-<GridItem col={35} row={4}>
-    <pre style={{color:"neutral"}}>
-         Delete User 
-    </pre>
-</GridItem>
 
-    
-<GridItem col={6} row={6}>
-    <pre style={{color:"green"}}>
-         Enter User ID: 
-    </pre>
-</GridItem>
+            <GridItem col={21} row={1}>
+                <pre style={{ color: "yellow" }}>
+                    {receivedData.title01}
+                </pre>
+            </GridItem>
 
-    
-<GridItem col={21} row={6}>
-    <Input maxLength={8} className='bms underLine' name='usridin' id='usridin' type='text' styles={{color:"green"}}  onChange={handleInputChange} onKeyDown={handleSubmit}/>
-</GridItem>
-    
-<GridItem col={30} row={6}>
-    <pre >
-          
-    </pre>
-</GridItem>
 
-    
-<GridItem col={6} row={8}>
-    <pre style={{color:"yellow"}}>
-         ********************************************************************** 
-    </pre>
-</GridItem>
+            <GridItem col={65} row={1}>
+                <pre style={{ color: "#7faded" }}>
+                    Date:
+                </pre>
+            </GridItem>
 
-    
-<GridItem col={6} row={11}>
-    <pre style={{color:"turquoise"}}>
-         First Name: 
-    </pre>
-</GridItem>
 
-    
-<GridItem col={18} row={11}>
-    <pre style={{color:"#7faded"}}>
-         {receivedData.fname } 
-    </pre>
-</GridItem>
+            <GridItem col={71} row={1}>
+                <pre style={{ color: "#7faded" }}>
+                    {receivedData.curdate}
+                </pre>
+            </GridItem>
 
-    
-<GridItem col={39} row={11}>
-    <pre >
-          
-    </pre>
-</GridItem>
 
-    
-<GridItem col={6} row={13}>
-    <pre style={{color:"turquoise"}}>
-         Last Name: 
-    </pre>
-</GridItem>
+            <GridItem col={1} row={2}>
+                <pre style={{ color: "#7faded" }}>
+                    Prog:
+                </pre>
+            </GridItem>
 
-    
-<GridItem col={18} row={13}>
-    <pre style={{color:"#7faded"}}>
-         {receivedData.lname } 
-    </pre>
-</GridItem>
 
-    
-<GridItem col={39} row={13}>
-    <pre style={{color:"green"}}>
-          
-    </pre>
-</GridItem>
+            <GridItem col={7} row={2}>
+                <pre style={{ color: "#7faded" }}>
+                    {receivedData.pgmname}
+                </pre>
+            </GridItem>
 
-    
-<GridItem col={6} row={15}>
-    <pre style={{color:"turquoise"}}>
-         User Type:  
-    </pre>
-</GridItem>
 
-    
-<GridItem col={17} row={15}>
-    <pre style={{color:"#7faded"}}>
-         {receivedData.usrtype } 
-    </pre>
-</GridItem>
+            <GridItem col={21} row={2}>
+                <pre style={{ color: "yellow" }}>
+                    {receivedData.title02}
+                </pre>
+            </GridItem>
 
-    
-<GridItem col={19} row={15}>
-    <pre style={{color:"#7faded"}}>
-         (A=Admin, U=User) 
-    </pre>
-</GridItem>
 
-    
-<GridItem col={1} row={23}>
-    <pre style={{color:"red"}}>
-         {receivedData.errmsg } 
-    </pre>
-</GridItem>
+            <GridItem col={65} row={2}>
+                <pre style={{ color: "#7faded" }}>
+                    Time:
+                </pre>
+            </GridItem>
 
-    
-<GridItem col={1} row={24}>
-    <pre style={{color:"yellow"}}>
-         ENTER=Fetch  F3=Back  F4=Clear  F5=Delete 
-    </pre>
-</GridItem>
 
-    
-    </>
-  );
+            <GridItem col={71} row={2}>
+                <pre style={{ color: "#7faded" }}>
+                    {receivedData.curtime}
+                </pre>
+            </GridItem>
+
+
+            <GridItem col={35} row={4}>
+                <pre style={{ color: "neutral" }}>
+                    Delete User
+                </pre>
+            </GridItem>
+
+
+            <GridItem col={6} row={6}>
+                <pre style={{ color: "green" }}>
+                    Enter User ID:
+                </pre>
+            </GridItem>
+
+
+            <GridItem col={21} row={6}>
+                <input maxLength={8} className='bms underLine' name='userId' id='userId' value={formData.userId} type='text' style={{ color: "green" }} onChange={handleInputChange} onKeyDown={handleSubmit} />
+            </GridItem>
+
+            <GridItem col={30} row={6}>
+                <pre >
+
+                </pre>
+            </GridItem>
+
+
+            <GridItem col={6} row={8}>
+                <pre style={{ color: "yellow" }}>
+                    **********************************************************************
+                </pre>
+            </GridItem>
+
+
+            <GridItem col={6} row={11}>
+                <pre style={{ color: "turquoise" }}>
+                    First Name:
+                </pre>
+            </GridItem>
+
+
+            <GridItem col={18} row={11}>
+                <pre style={{ color: "#7faded" }}>
+                    {receivedData.firstName}
+                </pre>
+            </GridItem>
+
+
+            <GridItem col={39} row={11}>
+                <pre >
+
+                </pre>
+            </GridItem>
+
+
+            <GridItem col={6} row={13}>
+                <pre style={{ color: "turquoise" }}>
+                    Last Name:
+                </pre>
+            </GridItem>
+
+
+            <GridItem col={18} row={13}>
+                <pre style={{ color: "#7faded" }}>
+                    {receivedData.lastName}
+                </pre>
+            </GridItem>
+
+
+            <GridItem col={39} row={13}>
+                <pre style={{ color: "green" }}>
+
+                </pre>
+            </GridItem>
+
+
+            <GridItem col={6} row={15}>
+                <pre style={{ color: "turquoise" }}>
+                    User Type:
+                </pre>
+            </GridItem>
+
+
+            <GridItem col={17} row={15}>
+                <pre style={{ color: "#7faded" }}>
+                    {receivedData.role}
+                </pre>
+            </GridItem>
+
+
+            <GridItem col={19} row={15}>
+                <pre style={{ color: "#7faded" }}>
+                    (A=Admin, U=User)
+                </pre>
+            </GridItem>
+
+
+            <GridItem col={1} row={23}>
+                <pre style={{ color: "red" }}>
+                    {receivedData.errmsg}
+                </pre>
+            </GridItem>
+
+
+            <GridItem col={1} row={24}>
+                <pre style={{ color: "yellow" }}>
+                    ENTER=Fetch  F3=Back  F4=Clear  F5=Delete
+                </pre>
+            </GridItem>
+
+
+        </>
+    );
 }
-    
